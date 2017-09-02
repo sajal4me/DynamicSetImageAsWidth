925356786d1d43c816e06d4d45b556e8bfc1d24b
Appinventiv Technologies <appinventiv@SG-Mac-MIni.local>
2017-09-02 16:20:05 +0530

    set column in a collection view dynamic

---
+++ DynamicSetImageAsWidth/CustomCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCell.swift
+//  DynamicSetImageAsWidth
+//
+//  Created by Appinventiv Technologies on 02/09/17.
+//  Copyright © 2017 Appinventiv Technologies. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell : UICollectionViewCell{
+    @IBOutlet weak var imageOutlet: UIImageView!
+    
+}
--- DynamicSetImageAsWidth/ViewController.swift
+++ DynamicSetImageAsWidth/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+    private let leftAndRightPadding:CGFloat = 32.0
+    private let numberOfItemPerRow : CGFloat = 3.0
+    private let heightAdjustment: CGFloat = 30.0
+    
+    var arr = [String]()
+    @IBOutlet weak var collectionViewOutlet: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collectionViewOutlet.delegate = self
+        collectionViewOutlet.dataSource = self
+        
+      arr = ["macbook","macbook-pro-13","macbook-pro-retina","macbook-air","imac-5k","mac-pro","mac-mini","display","airport-extreme","os-x","ilife","iwork", "logic-pro-x","final-cut-pro-x","aperture","ipod-nano","ipod-shuffle","ipod-touch","itunes","apple-tv","icloud","apple Pay"]
+        
+        let width = ((collectionViewOutlet!.frame.width) - leftAndRightPadding) / numberOfItemPerRow
+        
+       let layout = collectionViewOutlet.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width:width, height: (width + heightAdjustment))
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return arr.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CustomCell
+        cell.imageOutlet.image = UIImage(named: arr[indexPath.row])
+        return cell
+    }
 
 }
+
+
 
